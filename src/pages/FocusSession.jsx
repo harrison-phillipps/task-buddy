8c10853c3e342ebeeff2cffe2b4e9b0e9a975be7
harrison.phillipps <harrison.phillipps@outlook.com>
2026-03-16 02:55:57 +0000

    File changes

--- src/pages/FocusSession.jsx
+++ src/pages/FocusSession.jsx
@@ -44,6 +44,7 @@ import OfflineIndicator from "../components/OfflineIndicator";
 import PomodoroTimer from "../components/focus/PomodoroTimer";
 import DistractionBlocker from "../components/focus/DistractionBlocker";
 import SessionMetrics from "../components/focus/SessionMetrics";
+import SpotifyPlayer from "../components/focus/SpotifyPlayer";
 
 // Dynamic encouragement based on user progress
 const getEncouragementMessages = (userProgress) => {
