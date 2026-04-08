73a1a855b7865585d70d335360a58e52943689a2
harrison.phillipps <harrison.phillipps@outlook.com>
2026-04-08 11:37:59 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import './App.css'
 import { Toaster } from "@/components/ui/sonner"
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClientInstance } from '@/lib/query-client'
-import { ThemeProvider } from 'next-themes'
 // VisualEditAgent removed - caused duplicate React chunk conflict
 // NavigationTracker removed - caused duplicate React chunk conflict
 import { pagesConfig } from './pages.config'
@@ -69,8 +68,7 @@ const AuthenticatedApp = () => {
 
 function App() {
   return (
-    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-      <AuthProvider>
+    <AuthProvider>
         <QueryClientProvider client={queryClientInstance}>
           <Router>
             <AuthenticatedApp />
@@ -78,7 +76,6 @@ function App() {
           <Toaster />
         </QueryClientProvider>
       </AuthProvider>
-    </ThemeProvider>
   );
 }
 
