7bac61dec1545c72940bfe9a1e5377c5929b3979
harrison.phillipps <harrison.phillipps@outlook.com>
2026-04-08 11:59:31 +0000

    File changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -16,8 +16,10 @@ export default defineConfig({
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+    dedupe: ['react', 'react-dom'],
   },
   optimizeDeps: {
     include: ['react', 'react-dom', 'react/jsx-runtime'],
+    force: true,
   },
 });
