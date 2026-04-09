efbab211ec50306929600c9c02161f968fde3f04
harrison.phillipps <harrison.phillipps@outlook.com>
2026-04-09 03:16:57 +0000

    File changes

--- src/vite.config.js
+++ src/vite.config.js
@@ -31,7 +31,7 @@ export default defineConfig({
   server: {
     allowedHosts: ['.'],
   },
-  cacheDir: 'node_modules/.vite_cache_v2',
+  cacheDir: 'node_modules/.vite_cache_v3',
   plugins: [singleReactPlugin(), base44()],
   resolve: {
     alias: {
